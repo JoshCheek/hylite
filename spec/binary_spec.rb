b48e912bf62ac68bae7c87e27f60022cb1b071cb
Josh Cheek <josh.cheek@gmail.com>
2016-12-04 10:05:48 -0600

    Make the executable

--- spec/binary_spec.rb
+++ spec/binary_spec.rb
@@ -1,10 +1,19 @@
+require 'open3'
+
+bin_dir     = File.expand_path '../bin', __dir__
+ENV["PATH"] = bin_dir + ":" + ENV["PATH"]
+
 RSpec.describe 'binaries' do
   describe 'hylite' do
-    it 'reads code from stdin, highlights it, prints it to stdout'
-      # $ cat my_code.rb | hylite
+    it 'reads code from stdin, highlights it, prints it to stdout' do
+      stdout, stderr, status = Open3.capture3 'hylite', stdin_data: '1+1'
+      expect(stderr).to be_empty
+      expect(status).to be_success
+      expect(stdout).to eq "uhm, idk"
+    end
 
     describe 'arguments' do
-      it 'prints a help screen for -h / --help'
+      it 'prints a help screen for -h / --help and exits successfully'
         # $ hylite --help
       describe 'language' do
         it 'defaults to Ruby'
@@ -12,6 +21,8 @@ RSpec.describe 'binaries' do
         it 'can be specified with -l, --language, or --lmisspelled'
         # $ cat my_code.c | hylite --lang c
       end
+
+      it 'prints a help screen for unknown args and exits with an error'
     end
   end
 
