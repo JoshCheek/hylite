3488619f526bcde0086c7acf96f8de8be6fb1df2
Josh Cheek <josh.cheek@gmail.com>
2016-12-04 10:18:05 -0600

    Binary highlights using CodeRay

--- spec/binary_spec.rb
+++ spec/binary_spec.rb
@@ -4,12 +4,21 @@ bin_dir     = File.expand_path '../bin', __dir__
 ENV["PATH"] = bin_dir + ":" + ENV["PATH"]
 
 RSpec.describe 'binaries' do
+  def format_of(ansi_str)
+    # eg CodeRay format "1+1" as:
+    # ["\e[1;34m", "1", "\e[0m", "+", "\e[1;34m", "1", "\e[0m", "\n"]
+    #  bold blue         off          bold blue        off
+    ansi_str.scan(/\e.*?m|[^\e]+/).map do |token|
+      token[0] == "\e" ? :color : token
+    end
+  end
+
   describe 'hylite' do
     it 'reads code from stdin, highlights it, prints it to stdout' do
       stdout, stderr, status = Open3.capture3 'hylite', stdin_data: '1+1'
       expect(stderr).to be_empty
       expect(status).to be_success
-      expect(stdout).to eq "uhm, idk"
+      expect(format_of stdout).to eq [:color, "1", :color, "+", :color, "1", :color, "\n"]
     end
 
     describe 'arguments' do
