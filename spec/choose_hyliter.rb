89f462b9dc655ad0b2ce67dd2094bac82f5669ac
Josh Cheek <josh.cheek@gmail.com>
2016-12-23 19:21:06 -0600

    Refactor specs

--- spec/choose_hyliter.rb
+++ spec/choose_hyliter.rb
@@ -2,47 +2,32 @@ require 'hylite/choose_hyliter'
 require 'rouge'
 
 RSpec.describe 'ChooseHyliter' do
-  def unavailable!(klass)
-    allow(klass).to receive(:new?).and_return(nil)
+  def assert_hilights!(name, unavailable:[])
+    Array(unavailable).each do |klass|
+      allow(klass).to receive(:new?).and_return(nil)
+    end
+
+    hyliter = Hylite::ChooseHyliter.call("#a { color: #FFF; }" , "css")
+    expect(hyliter.type).to eq name
+
+    hylited = hyliter.call
+    expect(hyliter.call).to include 'FFF'
+    expect(hyliter.call).to include "\e[" # an escape sequence in there somewhere
   end
 
   it 'uses rouge if available' do
-    hyliter = Hylite::ChooseHyliter.call("#a { color: #FFF; }" , "css")
-    expect(hyliter.type).to eq :rouge
-
-    hylited = hyliter.call
-    expect(hyliter.call).to include 'FFF'
-    expect(hyliter.call).to include "\e[" # an escape sequence in there somewhere
+    assert_hilights! :rouge
   end
-
 
   it 'does not use rouge if it does not support the requested language'
 
-
   it 'uses pygments if rouge, isn\'t available' do
-    unavailable! Hylite::Rouge
-
-    hyliter = Hylite::ChooseHyliter.call("#a { color: #FFF; }" , "css")
-    expect(hyliter.type).to eq :pygments
-
-    hylited = hyliter.call
-    expect(hyliter.call).to include 'FFF'
-    expect(hyliter.call).to include "\e[" # an escape sequence in there somewhere
+    assert_hilights! :pygments, unavailable: [Hylite::Rouge]
   end
-
 
   it 'uses coderay if rouge and pygmentize aren\'t available' do
-    unavailable! Hylite::Rouge
-    unavailable! Hylite::Pygments
-
-    hyliter = Hylite::ChooseHyliter.call("#a { color: #FFF; }" , "css")
-    expect(hyliter.type).to eq :coderay
-
-    hylited = hyliter.call
-    expect(hyliter.call).to include 'FFF'
-    expect(hyliter.call).to include "\e[" # an escape sequence in there somewhere
+    assert_hilights! :coderay, unavailable: [Hylite::Rouge, Hylite::Pygments]
   end
-
 
   it 'tells you how to install these other libs if none of them are available'
 end
